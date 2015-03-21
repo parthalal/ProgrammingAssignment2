768175f908db623af3da9cd0264479394ef96c72
parthalal <partha.lal@gmail.com>
2015-03-21 22:39:54 +0000

    Revert "Implemented the functions as required..."
    This reverts commit 64f5b70a70ea73de62f99b603cc63d585f0799a9.

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,15 @@
-## returns a list of functions that caches the inverse of the matrix originally set
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setinv <- function(inv) m <<- inv
-  getinv <- function() m
-  list(set = set, get = get,
-       setinv = setinv,
-       getinv = getinv)
+
 }
 
 
-## Returns the inverse of x, using a cached copy if it exists and then caching the inverse for later use
+## Write a short comment describing this function
+
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  m <- x$getinv()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinv(m)
-  m
+        ## Return a matrix that is the inverse of 'x'
 }
